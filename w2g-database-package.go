11d77184369494af80ee3e7fe48066de023bc2cb
kryhin <artem@maxtvmedia.com>
2020-09-07 20:17:06 -0400

    FindInSlice added

--- w2g-database-package.go
+++ w2g-database-package.go
@@ -44,3 +44,12 @@ func ConvertStructField2DatabaseField(s string) string {
 
 	return field
 }
+
+func FindInSlice(slice []string, val string) (int, bool) {
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+	return -1, false
+}
